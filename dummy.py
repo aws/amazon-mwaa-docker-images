57e733afe25c12c4b4118afaf60bb51423b939f6
Rafid K <rafidka@gmail.com>
2023-12-18 17:25:16 -0800

    Update README and introduce linting (#1)

---
+++ dummy.py
@@ -0,0 +1,11 @@
+"""Dummy Python file to test Flake8 integration."""
+
+
+def main(msg: str) -> None:
+    """Entry point of the script."""
+    print("This is a dummy script to test Flake8 integration.")
+    print(f"Printing user message: {msg}")
+
+
+if __name__ == '__main__':
+    main("Hello!")
