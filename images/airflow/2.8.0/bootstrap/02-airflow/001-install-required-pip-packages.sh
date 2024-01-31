ca4220b77fbd8138a126699ac71c58d4370dbe50
Rafid K <rafidka@gmail.com>
2024-01-31 23:41:08 +0000

    Implement `safe-pip-install` command
    More information about this command can be found in #18.

---
+++ images/airflow/2.8.0/bootstrap/02-airflow/001-install-required-pip-packages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# shellcheck source=images/airflow/2.8.0/bootstrap/common.sh
+source /bootstrap/common.sh
+
+# safe-pip-install always install all required packages, along with whatever
+# the user provides, hence we don't need to provide anything here.
+safe-pip-install
