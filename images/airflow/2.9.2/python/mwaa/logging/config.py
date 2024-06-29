530ef2b44451109bbbc288743dfbed1a99764902
Rafid K <rafidka@gmail.com>
2024-06-29 21:25:16 +0000

    Fix task logs spilling into scheduler logs
    In our logging configuration, we were not setting the propagation flag
    to False for the `airflow.task` logger, resulting in some task logs
    going to the scheduler as well (and probably workers, but I didn't
    verify) because the go to the 'root' handler, which prints to console,
    and the latter gets captured by the scheduler.

--- images/airflow/2.9.2/python/mwaa/logging/config.py
+++ images/airflow/2.9.2/python/mwaa/logging/config.py
@@ -100,6 +100,7 @@ def _configure_task_logging():
         LOGGING_CONFIG["loggers"]["airflow.task"].update(
             {
                 "level": log_level,
+                "propagate": False,
             }
         )
 
