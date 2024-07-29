099c1dbfba09d7302ff09f09b4d3afacbc8e389a
Rafid K <rafidka@gmail.com>
2024-07-29 18:55:07 +0000

    Fix issue 123: Repeated reserialization of DAGs
    Issue #124 happens because we are always calling `airflow db migrate` to
    make sure the database is migrated. The assumption was that the
    operation is a no-op if the DB is already initialized. However, this is
    not the case, and a reserialization of DAGs is happening every time this
    command is called. To avoid this, I am changing the code for executing
    migration to check if migration is needed.

--- images/airflow/2.9.2/python/mwaa/entrypoint.py
+++ images/airflow/2.9.2/python/mwaa/entrypoint.py
@@ -96,7 +96,6 @@ USER_REQUIREMENTS_MAX_INSTALL_TIME = timedelta(minutes=9)
 CONTAINER_START_TIME = time.time()
 
 
-@with_db_lock(1234)
 async def airflow_db_init(environ: dict[str, str]):
     """
     Initialize Airflow database.
@@ -105,13 +104,9 @@ async def airflow_db_init(environ: dict[str, str]):
     function does this. This function is called in the entrypoint to make sure that,
     for any Airflow component, the database is initialized before it starts.
 
-    This function uses a DB lock to make sure that no two processes execute this
-    function at the same time.
-
     :param environ: A dictionary containing the environment variables.
     """
-    logger.info("Calling 'airflow db migrate' to initialize the database.")
-    await run_command("airflow db migrate", env=environ)
+    await run_command("python3 -m mwaa.database.migrate", env=environ)
 
 
 @with_db_lock(4321)
