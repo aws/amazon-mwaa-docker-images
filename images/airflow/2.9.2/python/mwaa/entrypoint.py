3f15a885e099bddde78c0add08444c8ef47ff470
Michael Fu <miklfu@amazon.com>
2024-11-13 22:59:11 +0000

    Add metric

--- images/airflow/2.9.2/python/mwaa/entrypoint.py
+++ images/airflow/2.9.2/python/mwaa/entrypoint.py
@@ -66,7 +66,7 @@ from mwaa.subprocess.conditions import (
 from mwaa.subprocess.subprocess import Subprocess, run_subprocesses
 from mwaa.utils.cmd import run_command
 from mwaa.utils.dblock import with_db_lock
-
+from mwaa.utils.statsd import get_statsd
 
 # Usually, we pass the `__name__` variable instead as that defaults to the
 # module path, i.e. `mwaa.entrypoint` in this case. However, since this is
@@ -128,25 +128,29 @@ async def increase_pool_size_if_default_size(environ: dict[str, str]):
     created_at = os.environ.get("MWAA__CORE__CREATED_AT")
     problematic_pool_size = 128
 
-
     if created_at:
-        date_format = "%a %b %d %H:%M:%S %Z %Y"
-        created_date = datetime.strptime(created_at, date_format)
-        # Has a little buffer from when 2.9.2 was released and when the fix was fully deployed
-        issue_beginning = datetime(2024, 7, 8)
-        issue_resolution = datetime(2024, 9, 6)
+        try:
+            date_format = "%a %b %d %H:%M:%S %Z %Y"
+            created_date = datetime.strptime(created_at, date_format)
+            # Has a little buffer from when 2.9.2 was released and when the fix was fully deployed
+            issue_beginning = datetime(2024, 7, 8)
+            issue_resolution = datetime(2024, 9, 6)
 
-        if created_date > issue_beginning and created_date < issue_resolution:
-            command_output = []
+            if created_date > issue_beginning and created_date < issue_resolution:
+                command_output = []
 
-            # Get the current default_pool size
-            await run_command("airflow pools get default_pool | grep default_pool | awk '{print $3}'",
-                               env=environ, stdout_logging_method=lambda output : command_output.append(output))
+                # Get the current default_pool size
+                await run_command("airflow pools get default_pool | grep default_pool | awk '{print $3}'",
+                                env=environ, stdout_logging_method=lambda output : command_output.append(output))
 
-            # Increasing the pool size if it is the default size
-            if len(command_output) == 1 and int(command_output[0]) == problematic_pool_size:
-                logger.info("Setting default_pool size to 10000.")
-                await run_command("airflow pools set default_pool 10000 default", env=environ)
+                # Increasing the pool size if it is the default size
+                if len(command_output) == 1 and int(command_output[0]) == problematic_pool_size:
+                    logger.info("Setting default_pool size to 10000.")
+                    await run_command("airflow pools set default_pool 10000 default", env=environ)
+                    stats = get_statsd()
+                    stats.incr("mwaa.pool.increased_default_pool_size", 1)
+        except Exception as error:
+            logger.error("Error checking if pool issue is present: " + error)
 
 @with_db_lock(4321)
 async def airflow_db_reset(environ: dict[str, str]):
