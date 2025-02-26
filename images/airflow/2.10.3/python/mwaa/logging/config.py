f9427c42d06ba5719612dbec2c0576f7a74e6bc8
Vraj2725 <126057146+Vraj2725@users.noreply.github.com>
2025-02-26 12:16:57 +0530

    Adding fix for incorrect bug and refactoring entrypoint.py to fix circular dependency issue (#215)
    *Issue #, if available:*
    When using airflow versions 2.9.2 and above customer are unable to see
    error logs when they set their loglevel to `WARNING` or above. The root
    cause is that in subprocess.py, the line
    `self.process_logger.info(line.decode("utf-8"))` overrides the log level
    coming from airflow stream to INFO in
    [subprocess.py](https://github.com/aws/amazon-mwaa-docker-images/blob/main/images/airflow/2.9.2/python/mwaa/subprocess/subprocess.py#L217).
    
    *Description of changes:*
    - Setting prcoess logger level to `DEBUG` to emit all logs and setting
    root logger to customer's log level to respect customer's log level.
    - Refactoring `entrypoint.py` and breaking down its logic into different
    files so all files have single responsibility such as:
         - `user_requirements.py`: Installing user requirements.
         - `execute_command.py`: Logic for executing all the subprocesses
    - `setup_environment.py`: Running the startup script and setting up
    environment variables
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.
    
    ---------
    
    Signed-off-by: Vraj2725 <126057146+Vraj2725@users.noreply.github.com>
    Co-authored-by: Vraj Patel <vrajtp@amazon.com>

--- images/airflow/2.10.3/python/mwaa/logging/config.py
+++ images/airflow/2.10.3/python/mwaa/logging/config.py
@@ -33,12 +33,17 @@ from airflow.config_templates.airflow_local_settings import (
 from mwaa.logging import cloudwatch_handlers
 from mwaa.utils import qualified_name
 
+CONSOLE_LOG_LEVEL = os.environ.get('AIRFLOW_CONSOLE_LOG_LEVEL', 'INFO')
 # We adopt the default logging configuration from Airflow and do the necessary changes
 # to setup logging with CloudWatch Logs.
 LOGGING_CONFIG = {
     **DEFAULT_LOGGING_CONFIG,
+    'root': {
+        'handlers': ['console'],
+        'level': CONSOLE_LOG_LEVEL,
+        'filters': ['mask_secrets'],
+    }
 }
-
 
 def _get_kms_key_arn():
     return os.environ.get("MWAA__CORE__KMS_KEY_ARN", None)
@@ -181,7 +186,7 @@ def _configure():
             comp,
             log_group_arn=log_group_arn,
             log_stream_name_prefix=comp.lower(),
-            log_level=log_level,
+            log_level="DEBUG",  # Customer Log Level handled at root logger
             logging_enabled=logging_enabled,
         )
         _configure_subprocesses_logging(
--- images/airflow/2.9.2/python/mwaa/logging/config.py
+++ images/airflow/2.9.2/python/mwaa/logging/config.py
@@ -33,10 +33,16 @@ from airflow.config_templates.airflow_local_settings import (
 from mwaa.logging import cloudwatch_handlers
 from mwaa.utils import qualified_name
 
+CONSOLE_LOG_LEVEL = os.environ.get('AIRFLOW_CONSOLE_LOG_LEVEL', 'INFO')
 # We adopt the default logging configuration from Airflow and do the necessary changes
 # to setup logging with CloudWatch Logs.
 LOGGING_CONFIG = {
     **DEFAULT_LOGGING_CONFIG,
+    'root': {
+        'handlers': ['console'],
+        'level': CONSOLE_LOG_LEVEL,
+        'filters': ['mask_secrets'],
+    }
 }
 
 
@@ -181,7 +187,7 @@ def _configure():
             comp,
             log_group_arn=log_group_arn,
             log_stream_name_prefix=comp.lower(),
-            log_level=log_level,
+            log_level="DEBUG",  # Customer Log Level handled at root logger
             logging_enabled=logging_enabled,
         )
         _configure_subprocesses_logging(
