72949094fdd5dc5c0c7aaeff27478747cd161f27
Sudipto Ghosh <hsudipto@amazon.com>
2025-03-21 10:30:10 +0530

    Raise exception when customer environment variables fail to load.

--- images/airflow/2.9.2/python/mwaa/entrypoint.py
+++ images/airflow/2.9.2/python/mwaa/entrypoint.py
@@ -376,7 +376,7 @@ def execute_startup_script(cmd: str, environ: Dict[str, str]) -> Dict[str, str]:
                 return customer_env_dict
             except Exception as e:
                 logger.error(f"Error reading the customer's environment variables: {e}")
-                return {}
+                raise Exception(f"Failed to read customer's environment variables from startup script: {e}")
 
         else:
             logger.error(
