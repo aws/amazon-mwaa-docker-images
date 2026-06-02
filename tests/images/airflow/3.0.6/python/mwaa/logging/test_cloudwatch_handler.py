eaaff5a4693c60156591aae22af0f1d6fcaadfe9
Andy Gao <chenhaog@amazon.com>
2026-06-02 23:25:48 +0000

    Enabling queue_circular for Fluent Handlers in 3.0.6 and 3.2.1

--- tests/images/airflow/3.0.6/python/mwaa/logging/test_cloudwatch_handler.py
+++ tests/images/airflow/3.0.6/python/mwaa/logging/test_cloudwatch_handler.py
@@ -189,7 +189,9 @@ def test_log_handler_creation(mock_boto3_client, mock_watchtower, mock_fluent, u
             mock_fluent.assert_called_once_with(
                 'customer.logs',
                 host=ANY,
-                port=24224
+                port=24224,
+                queue_maxsize=50000,
+                queue_circular=True,
             )
 
 def test_cloudwatch_remote_task_logger_initialization(mock_boto3_client):
@@ -414,7 +416,9 @@ def test_subprocess_log_handler_with_fluent(mock_boto3_client, mock_fluent):
         assert mock_fluent.call_args.args[0] == 'customer.logs'
         assert mock_fluent.call_args.kwargs == {
             'host': ANY,
-            'port': 24224
+            'port': 24224,
+            'queue_maxsize': 50000,
+            'queue_circular': True,
         }
 
 def test_dag_processor_manager_log_handler(mock_boto3_client, mock_fluent, mock_watchtower):
@@ -435,7 +439,9 @@ def test_dag_processor_manager_log_handler(mock_boto3_client, mock_fluent, mock_
         assert mock_fluent.call_args.args[0] == 'customer.logs'
         assert mock_fluent.call_args.kwargs == {
             'host': ANY,
-            'port': 24224
+            'port': 24224,
+            'queue_maxsize': 50000,
+            'queue_circular': True,
         }
 
 def test_dag_processing_log_handler(mock_boto3_client, mock_fluent, mock_watchtower):
@@ -458,7 +464,9 @@ def test_dag_processing_log_handler(mock_boto3_client, mock_fluent, mock_watchto
         assert mock_fluent.call_args.args[0] == 'customer.logs'
         assert mock_fluent.call_args.kwargs == {
             'host': ANY,
-            'port': 24224
+            'port': 24224,
+            'queue_maxsize': 50000,
+            'queue_circular': True,
         }
 
 
